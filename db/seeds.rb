f3530d37e732524e9c3b6bc84946468a835d2de1
AEH <escamilla.a@gmail.com>
2015-10-30 15:18:56 -0600

    seed data only 3 users + 50 random items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create a user
+user_1 = User.new(
+  name:     'User 1',
+  email:    'escamilla.a@gmail.com',
+  password: 'helloworld',
+)
+user_1.skip_confirmation!
+user_1.save!
+
+user_2 = User.new(
+  name:     'User 2',
+  email:    'user_2@example.com',
+  password: 'helloworld',
+)
+user_2.skip_confirmation!
+user_2.save!
+
+user_3 = User.new(
+  name:     'User 3',
+  email:    'user_3@example.com',
+  password: 'helloworld',
+)
+user_3.skip_confirmation!
+user_3.save!
+
+users = User.all
+
+50.times do
+  item = Item.create!(
+    user:   users.sample,
+    name:  Faker::Lorem.sentence,
+  )
+end
+
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
